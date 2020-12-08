b0fd78edc11d02ec5ce1ed4c39f7f2d1563a682b
松本公彰 <gph20078@s.okadai.jp>
2020-12-09 00:14:42 +0900

    ツイート機能を追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  
+  root 'tweets#index'  # 追加
+
+  devise_for :users
+  resources :tweets  # 追加
+  resources :users  # 追加
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
